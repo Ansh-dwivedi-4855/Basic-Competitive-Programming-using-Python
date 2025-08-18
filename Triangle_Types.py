6140e2f292e93469a24aef5c19512e6385b55170
Ansh-dwivedi-4855 <anshdwivedi4855@gmail.com>
2025-08-18 17:24:30 +0530

    Triangle_Types.py

---
+++ Triangle_Types.py
@@ -0,0 +1,16 @@
+# Program to check type of triangle using angles
+
+a = int(input("Enter first angle: "))
+b = int(input("Enter second angle: "))
+c = int(input("Enter third angle: "))
+
+# check if triangle is valid
+if a + b + c == 180 and a > 0 and b > 0 and c > 0:
+    if a == 90 or b == 90 or c == 90:
+        print("Right Triangle")
+    elif a > 90 or b > 90 or c > 90:
+        print("Obtuse Triangle")
+    else:
+        print("Acute Triangle")
+else:
+    print("Not a valid triangle")
