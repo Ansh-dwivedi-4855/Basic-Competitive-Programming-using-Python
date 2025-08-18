63a45c65a37a88bcf38378f96807e985b3c38295
Ansh-dwivedi-4855 <anshdwivedi4855@gmail.com>
2025-08-18 17:18:26 +0530

    Q1_divby7.py

---
+++ Q1_divby7.py
@@ -0,0 +1,8 @@
+# check whether the no. is divisible by 7 or not
+
+num = int(input("Enter a number: "))
+
+if num % 7 == 0:
+    print(f"{num} is divisible by 7")
+else:
+    print(f"{num} is not divisible by 7")
