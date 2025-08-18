5b2b9ec84218e1f0ef05e9c6bbec4848a223e451
Ansh-dwivedi-4855 <anshdwivedi4855@gmail.com>
2025-08-18 17:15:55 +0530

    Divisibilityby5N11.py

--- Divisibilityby5N11.py
+++ Divisibilityby5N11.py
@@ -1,4 +1,4 @@
-num = int(input("Enter The no."))
+#1.  Take an integer A as input. You have to tell whether A is divisible by both 5 and 11 or not. 
 
 if num % 5 == 0 and num % 11 == 0 :
     print("Yes it is divisible By 5 and 11 both")
